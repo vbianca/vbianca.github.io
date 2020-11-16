f3ca42f6b050b6bfb07ecb86168d54e5e51108bd
vbianca <bianca.nimigean@yahoo.com>
2020-11-16 19:17:33 +0200

    remove custom functions and use showPage from html

--- functions.js
+++ functions.js
@@ -1,39 +1,49 @@
 function hide(id){
     //document.getElementById(id).style.display = "none";
     var el = document.getElementById(id);
-    console.info("hide:" + id,el);
+    //console.info("hide:" + id,el);
   
     if (el){
         el.style.display = "none";
-    }else {
+    }
+    else {
         console.error("Pagina nu exista!",id)
     }
 }
-
 function hideAllPages(){
-    hide("home");
-    hide("skills");
-    hide("projects");
-    hide("languages");
+    //hide("home");
+    //hide("skills");
+    //hide("projects");
+    //hide("languages");
+
+    var pages = document.querySelectorAll(".page");
+    //for(initializare; conditie; post exectutie( i = i + 1))
+    for (var i = 0; i < pages.length; i++){
+        //var pages = pages[i];
+        var id = pages[i].id;
+         //console.info("i=", i, id, pages[i]);
+         hide(id);
+    }
 }
 
+function showPage(pageId) {
+    hideAllPages();
+    document.getElementById(pageId).style.display = "";
+ }
+/*
 function showHome() {
-   hideAllPages();
-    document.getElementById('home').style.display = '';
+   showPage('home');
 }
-
 
 function showSkills() {
-    hideAllPages();
-    document.getElementById('skills').style.display = '';
+    showPage('skills');
 }
 
 function showProjects(){
-    hideAllPages();
-    document.getElementById("projects").style.display = '';
+    showPage("projects");
 }
 
 function showLanguages(){
-    hideAllPages();
-    document.getElementById("languages").style.display = '';
+   showPage("languages");
 }
+*/
