cda1af09d46c6f913714cf5e5aadf51e69dc9eb0
vbianca <bianca.nimigean@yahoo.com>
2020-12-07 19:27:49 +0200

    refactor using getHTML

--- functions.js
+++ functions.js
@@ -41,24 +41,28 @@ initMenu();
 
 showPage(activePage);
 
-
-
-function showSkills(skills) {
+function getHTMLSkills(skills){
     var skillsLi = skills.map(function (skill) {
         var endorsements = ` <span>&middot; ${skill.endorsements}</span>`;
         return "<li>" + skill.name + endorsements + "</li>";
     });
+       return skillsLi.join("");
+}
+
+function showSkills(skills) {
+   var html = getHTMLSkills(skills);
     
-    
+
+   
+    //todo sort by endorsements
     var ul = document.querySelector("#skills ul");
-    ul.innerHTML = skillsLi.join("");
+    ul.innerHTML = html;
     
 }
 
 fetch("data/skills.json").then(function(r){
     return r.json();
 }).then(function(allSkills){
-    console.info('allSkills', allSkills);
     showSkills(allSkills);
 });
 
